0344b90019bc67a142f0ce8012a94975fd37c85b
Erdinch <erdinchorhan02@gmail.com>
2022-12-05 17:49:56 +0200

    categories now working

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const categoriesRouter = require('./routes/categories');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +26,8 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/category', categoriesRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ controllers/categoryController.js
@@ -0,0 +1,46 @@
+const Category = require('../models/category');
+const Item = require('../models/item');
+
+exports.index = (req, res) => {
+  Category.find({}, 'name')
+    .sort({ name: 1 })
+    .exec((err, results) => {
+      if (err) {
+        return next(err);
+      } else {
+        let categoriesCount = 0;
+        // convert first letter of category to uppercase
+        results.forEach((x) => {
+          x.name = x.name.charAt(0).toUpperCase() + x.name.slice(1);
+          categoriesCount += 1;
+        });
+        res.render('index', {
+          title: 'Inventory Manager',
+          categories: results,
+          count: categoriesCount,
+        });
+      }
+    });
+};
+
+exports.category_detail = (req, res) => {
+  Item.find({ category: req.params.id })
+    .sort({ name: 1 })
+    .exec(async (err, results) => {
+      if (err) {
+        return next(err);
+      } else {
+        let itemCount = 0;
+        results.forEach((item) => {
+          itemCount += 1;
+        });
+        const currentCategory = await Category.findById(req.params.id).exec();
+
+        res.render('category_detail', {
+          title: currentCategory.name,
+          items: results,
+          count: itemCount,
+        });
+      }
+    });
+};
