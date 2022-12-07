7f4e4cc5b2bdafae77dc64756932441ad33c4fd1
Erdinch <erdinchorhan02@gmail.com>
2022-12-07 19:33:57 +0200

    create and delete item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,8 @@
 const Category = require('../models/category');
 const Item = require('../models/item');
+const { body, validationResult } = require('express-validator');
 
-exports.item_detail = (req, res) => {
+exports.item_detail = (req, res, next) => {
   Item.findById(req.params.id).exec(async (err, results) => {
     if (err) {
       return next(err);
@@ -17,7 +18,95 @@ exports.item_detail = (req, res) => {
         price: results.price,
         stock: results.numberInStock,
         category: currentCategory,
+        itemID: req.params.id,
       });
     }
   });
+};
+
+exports.item_create = async (req, res, next) => {
+  try {
+    const category = await Category.findById(req.params.id);
+
+    if (category) {
+      res.render('item_create', {
+        title: 'Create Item',
+        category: category.name,
+      });
+    } else {
+      res.status(404).send("Page doesn't exist");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.item_post = [
+  body('itemName', 'Item name required').trim().isLength({ min: 3 }).escape(),
+  body('description').trim().escape(),
+  body('price', 'Price requirted').trim().isNumeric().escape(),
+  body('numberInStock', 'Number in stock requirted')
+    .trim()
+    .isNumeric()
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const name = req.body.itemName;
+    const description = req.body.description;
+    const price = req.body.price;
+    const numberInStock = req.body.numberInStock;
+
+    const category = req.params.id;
+
+    const item = new Item({
+      name: name,
+      description: description,
+      price: price,
+      numberInStock: numberInStock,
+      category: category,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('item_create', {
+        title: 'Create Category',
+        item,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Item with same name already exists.
+      Item.findOne({ name: name }).exec((err, found_item) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_item) {
+          // Item exists, redirect to its detail page.
+          res.redirect(found_item.url);
+        } else {
+          item.save((err) => {
+            if (err) {
+              return next(err);
+            }
+          });
+          // Item saved. Redirect to item detail page.
+          res.redirect(item.url);
+        }
+      });
+    }
+  },
+];
+
+exports.item_delete = async (req, res, next) => {
+  try {
+    await Item.deleteOne({ _id: req.params.id });
+
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 };
--- routes/item.js
+++ routes/item.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const itemController = require('../controllers/itemController');
 
+router.post('/create/:id', itemController.item_post);
+
+router.get('/create/:id', itemController.item_create);
+
+router.post('/:id', itemController.item_delete);
+
 router.get('/:id', itemController.item_detail);
 
 module.exports = router;
