aac46e1d625643f793a94b056cae35f9b03102c1
Erdinch <erdinchorhan02@gmail.com>
2022-12-07 19:34:03 +0200

    small fix

--- routes/categories.js
+++ routes/categories.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const categoryController = require('../controllers/categoryController');
 
-router.get('/create', categoryController.create_get);
-
 router.post('/create', categoryController.create_post);
 
-router.get('/delete/:id', categoryController.delete_get);
+router.get('/create', categoryController.create_get);
 
 router.post('/delete/:id', categoryController.delete_post);
+
+router.get('/delete/:id', categoryController.delete_get);
 
 router.get('/:id', categoryController.category_detail);
 
