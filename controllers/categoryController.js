7f48e01ef04d49b3998d2458c5b898f13b4ab16e
Erdinch <erdinchorhan02@gmail.com>
2022-12-07 18:05:17 +0200

    create delete categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 const Category = require('../models/category');
 const Item = require('../models/item');
+const { body, validationResult } = require('express-validator');
 
 exports.index = (req, res) => {
   Category.find({}, 'name')
@@ -23,7 +24,78 @@ exports.index = (req, res) => {
     });
 };
 
-exports.category_detail = (req, res) => {
+exports.create_get = (req, res) => {
+  res.render('create_category_form', {
+    title: 'Create Category',
+  });
+};
+
+exports.create_post = [
+  body('cName', 'Category name required').trim().isLength({ min: 3 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({ name: req.body.cName });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('create_category_form', {
+        title: 'Create Category',
+        category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Category with same name already exists.
+      Category.findOne({ name: req.body.name }).exec((err, found_category) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_category) {
+          // Category exists, redirect to its detail page.
+          res.redirect(found_category.url);
+        } else {
+          category.save((err) => {
+            if (err) {
+              return next(err);
+            }
+            // Category saved. Redirect to category detail page.
+            res.redirect(category.url);
+          });
+        }
+      });
+    }
+  },
+];
+
+exports.delete_get = (req, res, next) => {
+  Category.findById(req.params.id).exec((err, results) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.render('delete_category_form', {
+        title: 'Delete Category',
+        category: results.name,
+      });
+    }
+  });
+};
+
+exports.delete_post = async (req, res, next) => {
+  try {
+    await Item.deleteMany({ category: req.params.id });
+    await Category.deleteOne({ _id: req.params.id });
+
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.category_detail = (req, res, next) => {
   Item.find({ category: req.params.id })
     .sort({ name: 1 })
     .exec(async (err, results) => {
@@ -43,6 +115,7 @@ exports.category_detail = (req, res) => {
           title: currentCategory.name,
           items: results,
           count: itemCount,
+          cID: req.params.id,
         });
       }
     });
--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const categoryController = require('../controllers/categoryController');
 
+router.get('/create', categoryController.create_get);
+
+router.post('/create', categoryController.create_post);
+
+router.get('/delete/:id', categoryController.delete_get);
+
+router.post('/delete/:id', categoryController.delete_post);
+
 router.get('/:id', categoryController.category_detail);
 
 module.exports = router;
